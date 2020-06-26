64e5a83452aa8f6e487821e8bba9ca063d2624f4
Philip Simonson <psimonson1988@gmail.com>
2020-06-26 16:32:34 -0400

    I think I fixed the second stage now just need to test it.

--- stage2.asm
+++ stage2.asm
@@ -12,49 +12,35 @@ start:
 	call print
 
 	cli
-	lgdt [gdt.pointer]
+	lgdt [gdt_desc]
 	mov eax, cr0
 	or al, 1
 	mov cr0, eax
-	sti
 
-	mov si, op_pmode2
-	call print
+	jmp CODE_SEG:init_pm
 
 	cli
 	hlt
 
+[bits 32]
+
+init_pm:
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov bp, 0x90000
+	mov sp, bp
+
+	jmp $
+
 %include "common.inc"
 %include "a20.inc"
+%include "gdt.inc"
 
 op_pmode db "Entering protected mode...",0
 op_pmode2 db "done!",13,10,0
 op_a20yes db "A20 is enabled.",13,10,0
 op_a20no db "A20 is disabled.",13,10,0
-
-; gdt stuff below
-gdt:
-.null: equ $-gdt
-	dw 0
-	dw 0
-	db 0
-	db 0
-	db 0
-	db 0
-.code: equ $-gdt
-	dw 0xffff
-	dw 0
-	db 0
-	db 0x9a
-	db 11001111b
-	db 0
-.data: equ $-gdt
-	dw 0xffff
-	dw 0
-	db 0
-	db 0x92
-	db 11001111b
-	db 0
-.pointer:
-	dw $-gdt-1
-	dd gdt
