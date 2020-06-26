2bbcd08fe2870e37851e3615d43304f222b0dc42
Philip Simonson <psimonson1988@gmail.com>
2020-06-26 19:07:26 -0400

    Updated second stage, made operation more clear.

--- stage2.asm
+++ stage2.asm
@@ -3,6 +3,16 @@
 ; =======================
 
 [org 0x0]
+[bits 16]
+
+global main
+main:
+	jmp 0:start
+	nop
+
+%include "common.inc"
+%include "a20.inc"
+%include "gdt.inc"
 
 start:
 	call a20_bios
@@ -14,12 +24,10 @@ start:
 	cli
 	lgdt [gdt_desc]
 	mov eax, cr0
-	or al, 1
+	or eax, 1
 	mov cr0, eax
 
 	jmp CODE_SEG:init_pm
-
-	cli
 	hlt
 
 [bits 32]
@@ -31,14 +39,10 @@ init_pm:
 	mov fs, ax
 	mov gs, ax
 	mov ss, ax
-	mov bp, 0x90000
-	mov sp, bp
+	mov ebp, 0x90000
+	mov esp, ebp
 
 	jmp $
-
-%include "common.inc"
-%include "a20.inc"
-%include "gdt.inc"
 
 op_pmode db "Entering protected mode...",0
 op_pmode2 db "done!",13,10,0
