8491c4fc234c898617e3e23e8bb970c48725946f
Philip Simonson <psimonson1988@gmail.com>
2020-06-26 03:58:27 -0400

    Updated gdt and added bochs rc file

--- stage2.asm
+++ stage2.asm
@@ -11,12 +11,12 @@ start:
 	mov si, op_pmode
 	call print
 
-;	cli
-;	lgdt [gdtr]
-;	mov eax, cr0
-;	or al, 1
-;	mov cr0, eax
-;	sti
+	cli
+	lgdt [gdt.pointer]
+	mov eax, cr0
+	or al, 1
+	mov cr0, eax
+	sti
 
 	mov si, op_pmode2
 	call print
@@ -33,26 +33,28 @@ op_a20yes db "A20 is enabled.",13,10,0
 op_a20no db "A20 is disabled.",13,10,0
 
 ; gdt stuff below
-gdt_start:
+gdt:
+.null: equ $-gdt
 	dw 0
 	dw 0
-gdt_code:
+	db 0
+	db 0
+	db 0
+	db 0
+.code: equ $-gdt
 	dw 0xffff
+	dw 0
 	db 0
 	db 0x9a
-	db 0xcf
+	db 11001111b
 	db 0
-gdt_data:
+.data: equ $-gdt
 	dw 0xffff
+	dw 0
 	db 0
 	db 0x92
-	db 0xcf
+	db 11001111b
 	db 0
-gdt_end:
-
-gdtr:
-	dw gdt_end-gdt_start-1
-	dq gdt_start
-
-CODE_SEG equ gdt_code-gdt_start
-DATA_SEG equ gdt_data-gdt_start
+.pointer:
+	dw $-gdt-1
+	dd gdt
