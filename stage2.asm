74ef586dadb802891b098bb4a64dd3a28d2d53f4
Philip Simonson <psimonson1988@gmail.com>
2020-07-01 16:10:57 -0400

    Fixed up stage 2 load kernel.

--- stage2.asm
+++ stage2.asm
@@ -8,6 +8,10 @@
 start:
 	mov [iBootDrive], dl
 
+	; set text mode (80x25)
+	mov ax, 0x0003
+	int 0x10
+
 	call a20_bios
 	call check_a20
 
@@ -18,9 +22,9 @@ start:
 
 	mov ax, 2
 	mov cx, 16
-	xor bx, bx
+	mov bx, load_segment
 	mov es, bx
-	mov bx, kernel_offset
+	mov bx, load_offset
 	call read_disk
 
 	; switch on protected mode
@@ -49,7 +53,7 @@ INIT_PM:
 	mov ebp, 0x90000
 	mov esp, ebp
 
-	call kernel_offset
+	call run_offset
 	hlt
 
 %include "common32.inc"
@@ -60,6 +64,8 @@ op_a20yes db "A20 is enabled.",10,13,0
 op_a20no db "A20 is disabled.",10,13,0
 op_progress db 0x2e,0
 op_failed db 10,13,"File not found!",10,13,0
-kernel_offset equ 0x1000
+load_segment equ 0x1000
+load_offset equ 0x0000
+run_offset equ 0x00010000
 
 %include "bs.inc"
