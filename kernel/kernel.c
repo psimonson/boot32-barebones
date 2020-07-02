17084313d3bc16e97dda3cac82339c0aade8444d
Markus Wichmann <nullplan@gmx.net>
2020-07-02 04:41:42 +0200

    Change Hello World back to string.
    The whole purpose is to identify the issue with strings, so might as
    well use them.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,17 +4,5 @@
 void kernel_main(void)
 {
 	term_init(BLUE, YELLOW);
-	print_char(-1, -1, 'H');
-	print_char(-1, -1, 'e');
-	print_char(-1, -1, 'l');
-	print_char(-1, -1, 'l');
-	print_char(-1, -1, 'o');
-	print_char(-1, -1, ' ');
-	print_char(-1, -1, 'W');
-	print_char(-1, -1, 'o');
-	print_char(-1, -1, 'r');
-	print_char(-1, -1, 'l');
-	print_char(-1, -1, 'd');
-	print_char(-1, -1, '!');
-	print_char(-1, -1, '\n');
+        print("Hello World!\n");
 }
