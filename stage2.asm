f503afc9774b78724343580dad67e3773667c307
Philip Simonson <psimonson1988@gmail.com>
2020-07-03 14:13:34 -0400

    Sectors per track corrected from 9 to 18 and using load_file. All works well now.

--- stage2.asm
+++ stage2.asm
@@ -18,11 +18,9 @@ start:
 
 	mov si, op_loading
 	call print
-;	call load_file
+	call load_file
 
 	; load kernel from sectors
-	mov ax, 10	; starting sector of kernel
-	mov cx, 2		; total sector containing kernel
 	mov bx, load_segment
 	mov es, bx
 	xor bx, bx
