d2e78ef67fe36c6b99775396ff7aa66da50dc1ca
Philip Simonson <psimonson1988@gmail.com>
2020-07-06 19:43:23 -0400

    Almost fixed the keyboard now, need to fix the keymap for my keyboard.

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -31,19 +31,51 @@ char kbdus_table[128] = {
  */
 static void keyboard_callback(registers_t *regs)
 {
+	static unsigned char multibyte = 0;
 	char scancode = inb(0x60);
 
-	if(scancode == '\b') {
-		backspace(key_buffer);
-		print_bkspc();
-	} else if(scancode == '\n') { // Enter/Return
-		user_input(key_buffer);
-		key_buffer[0] = '\0';
+	multibyte = scancode;
+	if(multibyte >= 0xe0) {
+		if(scancode >= 0x80) {
+			/* TODO: Handle two byte release. */
+		} else {
+			/* TODO: Handle two byte press. */
+		}
+		multibyte = 0;
+	} else if(multibyte == 0xe1) {
+		switch(scancode) {
+			case 0x1d:
+			case 0x45:
+			case 0x9d:
+			case 0xe1:
+				/* TODO: Verify correct order */
+			break;
+			case 0xc5:
+				/* TODO: Handle pause key */
+				multibyte = 0;
+			break;
+			default:
+				/* Shouldn't happen, perhaps warn user. */
+				multibyte = 0;
+			break;
+		}
 	} else {
-		char letter = kbdus_table[(int)scancode];
-		char str[2] = {letter, '\0'};
-		append(key_buffer, letter);
-		print(str);
+		if(scancode >= 0x80) {
+			/* TODO: Handle one byte release. */
+		} else {
+			if(scancode == '\b') { // Backspace
+				backspace(key_buffer);
+				print_bkspc();
+			} else if(scancode == '\n') { // Enter/Return
+				user_input(key_buffer);
+				key_buffer[0] = '\0';
+			} else {
+				char letter = kbdus_table[multibyte];
+				char str[2] = {letter, '\0'};
+				append(key_buffer, letter);
+				print(str);
+			}
+		}
 	}
 	(void)regs;
 }
