d02ec1eb74669a161732af404fd33d542b42f061
Philip Simonson <psimonson1988@gmail.com>
2020-12-09 18:34:54 -0500

    Keyboard code has an error in the implementation (need to fix)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,6 +9,7 @@
 
 #include <stdbool.h>
 
+#include "kernel.h"
 #include "isr.h"
 #include "vga.h"
 #include "helper.h"
@@ -18,6 +19,7 @@
 
 char key_buffer[256]; // Used to store keyboard input.
 bool kbd_istyping;
+bool login_active;
 
 extern int get_command(char *buf, int size);
 
@@ -25,22 +27,30 @@ extern int get_command(char *buf, int size);
  */
 void kernel_main(void)
 {
+	// Initialize the terminal and install ISRs and IRQs.
+	term_init(BLUE, YELLOW);
+	isr_install();
+	irq_install();
+	
 	// Display welcome message to user and prompt.
-	kprintf("Welcome to my bare bones example 32 bit operating system.\n");
-	kprintf("\n  I have designed this because I would like to teach beginners\n");
-	kprintf("the proper way of doing things. As far as a beginning operating\n");
-	kprintf("system goes. Please feel free to clone and use it. If you are\n");
-	kprintf("are wondering about what license. It has an MIT license so\n");
-	kprintf("basically don't say you wrote it and you'll be fine. Other than\n");
-	kprintf("that, you can use it for whatever you want. Also this software\n");
-	kprintf("comes WITHOUT ANY WARRANTY of any kind. I will not be held\n");
-	kprintf("accountable for any damage this software may cause. Please use\n");
-	kprintf("it at your own risk.\n\n");
-	kprintf("Cheers,\nPhilip R. Simonson (aka 5n4k3)\n\n");
-	kprintf("Welcome to a bare bones kernel! This is a temporary shell.\n");
-	kprintf("Please type 'help' to see what you can do.\n\n");
-	while(get_command(key_buffer, sizeof(key_buffer)) > 0)
-		process_command(key_buffer);
+	kprintf(WELCOME_MESSAGE "? ");
+	for(;;) {
+		if(login_active) {
+			if(get_command(key_buffer, sizeof(key_buffer)) > 0)
+				if(!strcmp("root071", key_buffer)) {
+					login_active = false;
+					kprintf("Login successful!\nPlease type 'help' "
+						"for a list of commands.\n\n> ");
+				} else {
+					login_active = true;
+					kprintf("Login failed!\nLOGIN? ");
+				}
+		} else {
+			kprintf("> ");
+			if(get_command(key_buffer, sizeof(key_buffer)) > 0)
+				process_command(key_buffer);
+		}
+	}
 }
 /* Get key from keyboard.
  */
@@ -61,7 +71,6 @@ int get_command(char *buf, int size)
 	bool buf_char = false;
 	int i = 0;
 
-	kprintf("> ");
 	while(i < size) {
 		buf_char = true;
 		key = getch();
@@ -99,12 +108,10 @@ int get_command(char *buf, int size)
 }
 /* Start of operating system.
  */
- void _start(void)
- {
- 	// Initialize the terminal and install ISRs and IRQs.
-	term_init(BLUE, YELLOW);
-	isr_install();
-	irq_install();
+void _start(void)
+{
+	// Initialize variables and start main routine
 	kbd_istyping = false;
- 	kernel_main();
- }
+	login_active = true;
+	kernel_main();
+}
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -10,6 +10,20 @@
 #ifndef _KERNEL_H_
 #define _KERNEL_H_
 
+#define WELCOME_MESSAGE \
+"Welcome to my bare bones example 32 bit operating system.\n" \
+"\n  I have designed this because I would like to teach beginners\n" \
+"the proper way of doing things. As far as a beginning operating\n" \
+"system goes. Please feel free to clone and use it. If you are\n" \
+"are wondering about what license. It has an MIT license so\n" \
+"basically don't say you wrote it and you'll be fine. Other than\n" \
+"that, you can use it for whatever you want. Also this software\n" \
+"comes WITHOUT ANY WARRANTY of any kind. I will not be held\n" \
+"accountable for any damage this software may cause. Please use\n" \
+"it at your own risk.\n\n" \
+"Cheers,\nPhilip R. Simonson (aka 5n4k3)\n\n" \
+"Welcome to a bare bones kernel! Please login to the system.\n\nLOGIN? " \
+
 /* Get user input and process it. */
 void user_input(char *input);
 
